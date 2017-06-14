9122bd3377bcef0e29412726d3930ae4794231cc
Rohit Dhumal <Rohit@Rohits-MacBook-Air.local>
2017-06-13 23:45:51 -0700

    Sample spring boot project structure

---
+++ ad-security/src/main/java/com/ad/security/AdSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.ad.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdSecurityApplication.class, args);
+	}
+}
